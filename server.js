a0fa6e81d8af657f0b3cfb2a92c84f852194385c
leothellama <48735860+leothellama@users.noreply.github.com>
2020-07-24 13:36:48 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const pagesRouter = require('./routes/pages');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', pagesRouter);
+
+mongoose.connect('mongodb://localhost:27017/ContactDb', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('MongoDb connection success... and Express server is running');
+
+    });
+})
